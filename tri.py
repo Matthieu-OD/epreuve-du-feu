520902e514607f0808ed8197347b547fd7259254
Matthieu-OD <matt.olenga@gmail.com>
2021-03-30 09:04:34 +0200

    added the tri algorithm

--- tri.py
+++ tri.py
@@ -1,1 +1,31 @@
-# I still have 2h13 to finish these exercises
+from sys import argv
+
+liste = [int(num) for num in argv[1:]]
+
+def tri(liste):
+    """
+        Take a list and return the list sorted
+    """
+
+    if len(liste) == 0 or len(liste) == 1:
+        return liste
+
+    mid_idx = len(liste) // 2;
+    mid_num = liste[mid_idx]
+
+    small_num_list = []
+    big_num_list = []
+
+    for idx in range(len(liste)):
+        num = liste[idx]
+
+        if idx == mid_idx:
+            continue
+
+        if num > mid_num:
+            big_num_list.append(num)
+        else:
+            small_num_list.append(num)
+    return tri(small_num_list) + [mid_num] + tri(big_num_list)
+
+print(tri(liste))
