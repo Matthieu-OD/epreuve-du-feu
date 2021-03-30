285ac7355d3c08e84ce45a5394c7666167a2d0f1
Matthieu-OD <matt.olenga@gmail.com>
2021-03-31 00:37:55 +0200

    added all the exercices except the sudoku

--- anagrammes.py
+++ anagrammes.py
@@ -3,11 +3,20 @@ from sys import argv
 def anagrammes(word, file):
     res = []
 
-     list_word = list(word)
-     list_word.sort()
+    list_word = list(word)
+    list_word.sort()
 
-     file_of_words = open(file, 'r')
+    file_of_words = open(file, 'r')
 
-     for anagrammes in file_of_words:
-         list_anagrammes = list(anagrames)
+    for anagramme in file_of_words:
+        list_anagrammes = list(anagramme[:len(anagramme)-1])
+        list_anagrammes.sort()
+
+        if list_anagrammes == list_word:
+            res.append(anagramme[:len(anagramme)-1])
+
+    file_of_words.close()
          
+    return res
+
+print(anagrammes(argv[1], argv[2]))
